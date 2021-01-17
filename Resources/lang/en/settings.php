143c8b0cedaa4532d95bb8721b526aaa87263752
tecnodesignc <marcos21.009@gmail.com>
2021-01-17 15:33:29 -0500

    Settings contact Site

--- Resources/lang/en/settings.php
+++ Resources/lang/en/settings.php
@@ -7,4 +7,13 @@ return [
     'template' => 'Front end template',
     'analytics-script' => 'Analytics Script',
     'locales' => 'Site locales',
-];
+    'Id facebook'=>'Id facebook',
+    'twitter Account'=>'Twitter Account',
+    'Address'=>'Address',
+    'mail'=>'E-Mail Accounts',
+    'phone'=> 'Phone Numbers',
+    'facebook'=> 'Facebook Account',
+    'instagram'=> 'Instagram Account',
+    'linkedin'=>'LinkedIn Account',
+    'Youtube'=>'Youtube Chanel'
+    ];
--- Resources/lang/es/settings.php
+++ Resources/lang/es/settings.php
@@ -7,4 +7,13 @@ return [
     'template' => 'Plantilla del sitio',
     'google-analytics' => 'Código de Google Analytics',
     'locales' => 'Idiomas',
+    'Id facebook'=>'Id facebook',
+    'twitter Account'=>'Cuenta de Twitter ',
+    'Address'=>'Direcciòn de la empresa',
+    'mail'=>'Cuentas de E-Mail',
+    'phone'=> 'Numeros de telefono',
+    'facebook'=> 'Cuenta de Facebook',
+    'instagram'=> 'Cuenta de Instagram',
+    'linkedin'=>'Cuenta de LinkedIn',
+    'Youtube'=>'CAnal de Youtube'
 ];
